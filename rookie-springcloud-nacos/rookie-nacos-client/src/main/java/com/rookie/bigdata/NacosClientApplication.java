11f0332bf9b01c657942b8a961a708484d2efe6e
liuxili01 <liuxili@bestseller.com.cn>
2024-09-20 16:30:50 +0800

    nacos client

---
+++ rookie-springcloud-nacos/rookie-nacos-client/src/main/java/com/rookie/bigdata/NacosClientApplication.java
@@ -0,0 +1,19 @@
+package com.rookie.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName NacosClientApplication
+ * @Description NacosClientApplication
+ * @Author rookie
+ * @Date 2021/6/17 11:32
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class NacosClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosClientApplication.class, args);
+    }
+}
---
+++ rookie-springcloud-nacos/rookie-nacos-client/src/main/java/com/rookie/bigdata/service/NacosClientService.java
@@ -0,0 +1,11 @@
+package com.rookie.bigdata.service;
+
+/**
+ * @Class NacosClientService
+ * @Description
+ * @Author rookie
+ * @Date 2024/9/20 16:14
+ * @Version 1.0
+ */
+public class NacosClientService {
+}
