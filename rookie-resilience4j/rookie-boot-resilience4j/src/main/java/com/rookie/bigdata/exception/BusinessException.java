8ee80d12b9ee6c33c01cd2951088e024adf0d337
albert.liu <albert.liu435@gmail.com>
2025-05-07 18:43:05 +0800

    resilience4j boot搭建

---
+++ rookie-resilience4j/rookie-boot-resilience4j/src/main/java/com/rookie/bigdata/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.rookie.bigdata.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
